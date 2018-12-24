f6a7f35b4a3dcd3c72aca285085216a64ed0e370
CursedFlames <18627001+CursedFlames@users.noreply.github.com>
2018-12-25 10:00:25 +1300

    placeholder textures for prism and gluttony pendant

--- src/main/java/cursedflames/bountifulbaubles/item/ItemPhantomPrism.java
+++ src/main/java/cursedflames/bountifulbaubles/item/ItemPhantomPrism.java
@@ -7,8 +7,9 @@ import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumHand;
 import net.minecraft.world.World;
+import vazkii.quark.api.ICustomEnchantColor;
 
-public class ItemPhantomPrism extends GenericItemBB {
+public class ItemPhantomPrism extends GenericItemBB implements ICustomEnchantColor {
 	public static final int GUI_ID = 3;
 
 	public ItemPhantomPrism() {
@@ -23,5 +24,15 @@ public class ItemPhantomPrism extends GenericItemBB {
 		player.openGui(BountifulBaubles.instance, GUI_ID, world, (int) player.posX,
 				(int) player.posY, (int) player.posZ);
 		return new ActionResult<ItemStack>(EnumActionResult.SUCCESS, player.getHeldItem(hand));
+	}
+
+	@Override
+	public boolean hasEffect(ItemStack stack) {
+		return true;
+	}
+
+	@Override
+	public int getEnchantEffectColor(ItemStack stack) {
+		return 0x5519AF;
 	}
 }
