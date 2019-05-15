96afdd8313083bcf3e23575d7ef70de791fe54f4
Tobias Hotz <tobias.hotz@hotmail.de>
2019-05-15 22:45:09 +0200

    Add compat of broken heart for firstaid

--- src/main/java/cursedflames/bountifulbaubles/item/ItemTrinketBrokenHeart.java
+++ src/main/java/cursedflames/bountifulbaubles/item/ItemTrinketBrokenHeart.java
@@ -6,6 +6,10 @@ import baubles.api.BaubleType;
 import baubles.api.BaublesApi;
 import cursedflames.bountifulbaubles.BountifulBaubles;
 import cursedflames.lib.config.Config.EnumPropSide;
+import ichttt.mods.firstaid.api.FirstAidRegistry;
+import ichttt.mods.firstaid.api.damagesystem.AbstractDamageablePart;
+import ichttt.mods.firstaid.api.damagesystem.AbstractPlayerDamageModel;
+import ichttt.mods.firstaid.api.event.FirstAidLivingDamageEvent;
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.ai.attributes.AttributeModifier;
 import net.minecraft.entity.ai.attributes.IAttributeInstance;
@@ -47,6 +51,7 @@ public class ItemTrinketBrokenHeart extends AGenericItemBauble {
 	public static void onDamage(LivingDamageEvent event) {
 		if (!(event.getEntity() instanceof EntityPlayer))
 			return;
+		if (FirstAidRegistry.getImpl() != null) return; //Firstaid loaded and present - see code below
 		EntityPlayer player = (EntityPlayer) event.getEntity();
 //		BountifulBaubles.logger.info("playerhit");
 		if (BaublesApi.isBaubleEquipped(player, ModItems.trinketBrokenHeart)==-1)
@@ -74,6 +79,44 @@ public class ItemTrinketBrokenHeart extends AGenericItemBauble {
 			event.setCanceled(true);
 		}
 		event.setAmount((float) (Math.max(event.getAmount()-maxHealthDamage, 0)));
+
+//		player.world.playSound(null, player.posX, player.posY, player.posZ,
+//				SoundEvents.ENTITY_PLAYER_HURT, SoundCategory.PLAYERS, 1.0F,
+//				(player.world.rand.nextFloat()-player.world.rand.nextFloat())*0.15F+0.75F);
+		player.world.playSound(null, player.posX, player.posY, player.posZ,
+				SoundEvents.ENTITY_IRONGOLEM_HURT, SoundCategory.PLAYERS, 0.7F,
+				(player.world.rand.nextFloat()-player.world.rand.nextFloat())*0.1F+0.8F);
+	}
+
+	@SubscribeEvent
+	public static void onFirstAidDamage(FirstAidLivingDamageEvent event) {
+		EntityPlayer player = event.getEntityPlayer();
+//		BountifulBaubles.logger.info("playerhit");
+		if (BaublesApi.isBaubleEquipped(player, ModItems.trinketBrokenHeart)==-1)
+			return;
+		AbstractPlayerDamageModel afterDamage = event.getAfterDamage();
+		if (!afterDamage.isDead(player)) return;
+//		BountifulBaubles.logger.info("health after damage "+healthAfterDamage);
+		double maxHealthDamage = event.getUndistributedDamage();
+		for (AbstractDamageablePart damageablePart : afterDamage) {
+			if ((damageablePart.canCauseDeath || afterDamage.hasNoCritical()) && damageablePart.currentHealth < 1F) {
+				maxHealthDamage += (1F - damageablePart.currentHealth);
+			}
+		}
+
+//		BountifulBaubles.logger.info("damage to maxHealth "+maxHealthDamage);
+		IAttributeInstance maxHealth = player
+				.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH);
+		AttributeModifier modifier = maxHealth.getModifier(MODIFIER_UUID);
+		double prevMaxHealthDamage = 0;
+		if (modifier!=null) {
+			prevMaxHealthDamage = modifier.getAmount();
+			maxHealth.removeModifier(modifier);
+		}
+		modifier = new AttributeModifier(MODIFIER_UUID, "Broken Heart MaxHP drain",
+				prevMaxHealthDamage-maxHealthDamage, 0);
+		maxHealth.applyModifier(modifier);
+		afterDamage.revivePlayer(player);
 
 //		player.world.playSound(null, player.posX, player.posY, player.posZ,
 //				SoundEvents.ENTITY_PLAYER_HURT, SoundCategory.PLAYERS, 1.0F,
