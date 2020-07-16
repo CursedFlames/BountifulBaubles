4cff8da2a6065cb0965f4dee63bc9317c478277f
CursedFlames <18627001+CursedFlames@users.noreply.github.com>
2020-07-16 14:52:58 +1200

    add all items with no functionality

--- src/main/java/cursedflames/bountifulbaubles/common/item/ModItems.java
+++ src/main/java/cursedflames/bountifulbaubles/common/item/ModItems.java
@@ -20,7 +20,7 @@ public class ModItems {
 	
 	public static final ItemGroup GROUP = FabricItemGroupBuilder.build(
 			modId(BountifulBaubles.MODID),
-			()->new ItemStack(Blocks.COBBLESTONE));
+			()->new ItemStack(ModItems.OBSIDIAN_SKULL));
 
 	private static Item add(String id, Item item) {
 		return add(modId(id), item);
@@ -40,10 +40,82 @@ public class ModItems {
 	}
 	
 	
-//	public static final Item TEST_ITEM = add("test_item",
-//			new BBItem(baseSettings()));
 	public static final Item BALLOON = add("balloon",
 			new BBItem(baseSettingsCurio()));
+	
+	public static final Item SUNGLASSES = add("sunglasses",
+			new BBItem(baseSettingsCurio()));
+	public static final Item APPLE = add("apple",
+			new BBItem(baseSettingsCurio()));
+	public static final Item VITAMINS = add("vitamins",
+			new BBItem(baseSettingsCurio()));
+	public static final Item RING_OVERCLOCKING = add("ring_overclocking",
+			new BBItem(baseSettingsCurio()));
+	public static final Item SHULKER_HEART = add("shulker_heart",
+			new BBItem(baseSettingsCurio()));
+	public static final Item RING_FREE_ACTION = add("ring_free_action",
+			new BBItem(baseSettingsCurio()));
+	public static final Item BEZOAR = add("bezoar",
+			new BBItem(baseSettingsCurio()));
+	public static final Item ENDER_DRAGON_SCALE = add("ender_dragon_scale",
+			new BBItem(baseSettings()));
+	public static final Item BROKEN_BLACK_DRAGON_SCALE = add("broken_black_dragon_scale",
+			new BBItem(baseSettings()));
+	public static final Item BLACK_DRAGON_SCALE = add("black_dragon_scale",
+			new BBItem(baseSettingsCurio()));
+	public static final Item MIXED_DRAGON_SCALE = add("mixed_dragon_scale",
+			new BBItem(baseSettingsCurio()));
+	public static final Item ANKH_CHARM = add("ankh_charm",
+			new BBItem(baseSettingsCurio()));
+	
+	public static final Item OBSIDIAN_SKULL = add("obsidian_skull",
+			new BBItem(baseSettingsCurio()));
+	public static final Item SHIELD_COBALT = add("shield_cobalt",
+			new BBItem(baseSettings().maxDamage(336*3)));
+	public static final Item SHIELD_OBSIDIAN = add("shield_obsidian",
+			new BBItem(baseSettings().maxDamage(336*4)));
+	public static final Item SHIELD_ANKH = add("shield_ankh",
+			new BBItem(baseSettings().maxDamage(336*5)));
+	
+	public static final Item MAGIC_MIRROR = add("magic_mirror",
+			new BBItem(baseSettings().maxCount(1)));
+	public static final Item POTION_RECALL = add("potion_recall",
+			new BBItem(baseSettings()));
+	public static final Item WORMHOLE_MIRROR = add("wormhole_mirror",
+			new BBItem(baseSettings().maxCount(1)));
+	public static final Item POTION_WORMHOLE = add("potion_wormhole",
+			new BBItem(baseSettings()));
+	
+	public static final Item LUCKY_HORSESHOE = add("lucky_horseshoe",
+			new BBItem(baseSettingsCurio()));
+	
+	public static final Item AMULET_SIN_EMPTY = add("amulet_sin_empty",
+			new BBItem(baseSettings()));
+	public static final Item AMULET_SIN_GLUTTONY = add("amulet_sin_gluttony",
+			new BBItem(baseSettingsCurio()));
+	public static final Item AMULET_SIN_PRIDE = add("amulet_sin_pride",
+			new BBItem(baseSettingsCurio()));
+	public static final Item AMULET_SIN_WRATH = add("amulet_sin_wrath",
+			new BBItem(baseSettingsCurio()));
+	
+	public static final Item BROKEN_HEART = add("broken_heart",
+			new BBItem(baseSettingsCurio()));
+	public static final Item PHYLACTERY_CHARM = add("phylactery_charm",
+			new BBItem(baseSettingsCurio()));
+	public static final Item AMULET_CROSS = add("amulet_cross",
+			new BBItem(baseSettingsCurio()));
+	
+	public static final Item GLOVES_DEXTERITY = add("gloves_dexterity",
+			new BBItem(baseSettingsCurio()));
+	public static final Item GLOVES_DIGGING_IRON = add("gloves_digging_iron",
+			new BBItem(baseSettingsCurio()));
+	public static final Item GLOVES_DIGGING_DIAMOND = add("gloves_digging_diamond",
+			new BBItem(baseSettingsCurio()));
+
+	public static final Item DISINTEGRATION_TABLET = add("disintegration_tablet",
+			new BBItem(baseSettings().maxCount(1)));
+	public static final Item SPECTRAL_SILT = add("spectral_silt",
+			new BBItem(baseSettings()));
 	
 	
 	public static void registerItems() {
