614a125c89188a1c932e1de178c911669305a564
CursedFlames <18627001+CursedFlames@users.noreply.github.com>
2021-07-18 19:42:05 +1200

    remove old forge code

--- forge/src/main/java/cursedflames/bountifulbaubles/forge/common/old/item/items/ItemHorseshoeBalloon.java
+++
@@ -1,9 +0,0 @@
-package cursedflames.bountifulbaubles.forge.common.old.item.items;
-
-import cursedflames.bountifulbaubles.forge.common.old.baubleeffect.EffectFallDamageResistNegate;
-
-public class ItemHorseshoeBalloon extends ItemBalloon implements EffectFallDamageResistNegate.IFallDamageNegateItem {
-	public ItemHorseshoeBalloon(String name, Settings props) {
-		super(name, props);
-	}
-}
